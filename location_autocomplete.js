e7422ac7cbba9a886c8baa39d1d7abad98481c66
Brandon Bergren <bdragon@rtk0.net>
2008-08-06 15:29:51 +0000

    #291583 by mongolito404, jslinted by me: Autocomplete for Drupal 6.x

--- location_autocomplete.js
+++ location_autocomplete.js
@@ -3,20 +3,16 @@
 /**
  * Twiddle the province autocomplete whenever the user changes the country.
  */
-if (Drupal.jsEnabled) {
-  $(document).ready(function() {
-    $('select.location_auto_country').change(function(e) {
-      var theinput = $(this).parents('fieldset').eq(0).find('.location_auto_province').unbind()[0];
-      // Clear province.
-      theinput.value = '';
-      // Unbind events.
-      var theid = '#' + theinput.id + '-autocomplete';
-      // Reset autocomplete status.
-      var input = $(theid).attr('autocomplete', 'ON')[0];
-      // Update autocomplete url for new country.
-      input.value = input.value.substr(0, input.value.length - 2) + this.value;
-      // Re-attach.
-      Drupal.autocompleteAutoAttach();
-    });
-  });
-}
+Drupal.behaviors.location = function(context) {
+  $('select.location_auto_country:not(.location-processed)', context).change(function(e) {
+    //Unbind events on province field and empty its value
+    var input = $('.location_auto_province', $(this).parents('fieldset:first')).unbind().val('');
+    //Get the (hidden) *-autocomplete input element
+    var input_autocomplete = $('#' + input.attr('id') + '-autocomplete');
+    // Update autocomplete url
+    input_autocomplete.val(input_autocomplete.val().substr(0, input_autocomplete.val().length - 2) + $(this).val());
+    // Re-attach events
+    input_autocomplete.removeClass('autocomplete-processed');
+    Drupal.behaviors.autocomplete($(this).parents('fieldset:first'));
+  }).addClass('location-processed');
+};
