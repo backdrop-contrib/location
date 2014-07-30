2a19305eaa5c341c62afe3877d97f1bfce986ff7
mrtoner <mrtoner@79398.no-reply.drupal.org>
2014-07-31 00:05:47 +0300

    Issue #1038164 by mrtoner, Rob_Feature: Added Wrap entire location display in a div.

--- locations.tpl.php
+++ locations.tpl.php
@@ -1,8 +1,10 @@
 <?php if (!empty($locations)): ?>
-  <h3 class="location-locations-header"><?php print format_plural(count($locations), 'Location', 'Locations'); ?></h3>
-  <div class="location-locations-wrapper">
-    <?php foreach ($locations as $location): ?>
-      <?php print $location; ?>
-    <?php endforeach; ?>
+  <div class="location-locations-display">
+    <h3 class="location-locations-header"><?php print format_plural(count($locations), 'Location', 'Locations'); ?></h3>
+    <div class="location-locations-wrapper">
+      <?php foreach ($locations as $location): ?>
+        <?php print $location; ?>
+      <?php endforeach; ?>
+    </div>
   </div>
 <?php endif; ?>
