9cde7bdb9a6c2b03d6971d44d7c580389809584d
root <podarok@ua.fm>
2014-08-19 15:48:41 +0300

    CodeSniffer for locations.tpl.php

--- locations.tpl.php
+++ locations.tpl.php
@@ -1,3 +1,10 @@
+<?php
+
+/**
+ * @file
+ * Template for displaying locations.
+ */
+?>
 <?php if (!empty($locations)): ?>
   <div class="location-locations-display">
     <h3 class="location-locations-header"><?php print format_plural(count($locations), 'Location', 'Locations'); ?></h3>
