95eb44838578248746f4cb3f67cf58615741cd7d
Sam-Inet <Sam-Inet@191508.no-reply.drupal.org>
2016-01-08 12:11:25 +0200

    Issue #1943912 by Michael-Inet, hvasconcelos, SeanA: Patch - ZIP Code showing in the wrong place

--- location.tpl.php
+++ location.tpl.php
@@ -20,19 +20,16 @@
         <?php endif; ?>
       </div>
     <?php endif; ?>
-    <?php if (!empty($postal_code)): ?>
-      <span class="postal-code" itemprop="postalCode"><?php print $postal_code; ?></span>
-    <?php endif; ?>
-    <?php if (!empty($city)): ?>
-      <span class="locality" itemprop="addressLocality">
-      <?php print $city; ?>
-      </span>
-      <?php if (!empty($province)) : ?>
-        <?php print ', '; ?>
-      <?php endif; ?>
+    <?php if (!empty($city) && !empty($province)): ?>
+      <span class="locality" itemprop="addressLocality"><?php print $city . ', '; ?></span>
+    <?php elseif (!empty($city)): ?>
+      <span class="locality" itemprop="addressLocality"><?php print $city; ?></span>
     <?php endif; ?>
     <?php if (!empty($province)): ?>
       <span class="region" itemprop="addressRegion"><?php print $province_print; ?></span>
+    <?php endif; ?>
+    <?php if (!empty($postal_code)): ?>
+      <span class="postal-code" itemprop="postalCode"><?php print $postal_code; ?></span>
     <?php endif; ?>
     <?php if (!empty($country_name)): ?>
       <div class="country-name" itemprop="addressCountry"><?php print $country_name; ?></div>
